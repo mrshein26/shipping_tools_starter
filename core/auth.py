33f6c2fdabd56afcddcac438d93f4f1fbc16ffb0
mrshein26 <sheinmon777@gmail.com>
2026-09-08 12:55:18 +0630

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -3,12 +3,12 @@ import streamlit as st
 # ဝန်ထမ်းစာရင်း (Database)
 # st.secrets မဆောက်ရသေးပါက Error မတက်စေရန် Default Password များ ထည့်ပေးထားပါသည်
 USER_DB = {
-    "admin": {"password": st.secrets.get("passwords", {}).get("admin", "admin123"), "status": "active"},
+    "admin": {"password": st.secrets.get("passwords", {}).get("admin", "admin2026@"), "status": "active"},
     "sheinmon": {"password": st.secrets.get("passwords", {}).get("sheinmon", "pass123"), "status": "active"},
-    "sulae": {"password": st.secrets.get("passwords", {}).get("sulae", "pass123"), "status": "active"},
-    "thaesumon": {"password": st.secrets.get("passwords", {}).get("thaesumon", "pass123"), "status": "active"},
-    "thetaunghtay": {"password": st.secrets.get("passwords", {}).get("thetaunghtay", "pass123"), "status": "active"},
-    "zayarphyohtut": {"password": st.secrets.get("passwords", {}).get("zayarphyohtut", "pass123"), "status": "active"}
+    "sulae": {"password": st.secrets.get("passwords", {}).get("sulae", "Sl12365"), "status": "active"},
+    "thaesumon": {"password": st.secrets.get("passwords", {}).get("thaesumon", "Tsm7890"), "status": "active"},
+    "thetaunghtay": {"password": st.secrets.get("passwords", {}).get("thetaunghtay", "Tah1111"), "status": "active"},
+    "zayarphyohtut": {"password": st.secrets.get("passwords", {}).get("zayarphyohtut", "Zyph1122"), "status": "active"}
 }
 
 def check_credentials(username, password):
